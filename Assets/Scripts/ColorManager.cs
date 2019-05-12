99db0e8b1aa6640dcf8c90312acffe11f9afe033
Pastew <kpastew@gmail.com>
2019-05-12 22:49:57 +0200

    More color managment

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -4,32 +4,49 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
-    private StarsUI starsUI;
+    public bool updateColorsInUpdate = false;
 
     public Color cameraColor;
 
+    GameObject goalPrefab;
+    public Color goalColor;
+
+    GameObject heroPrefab;
+    public Color heroColor;
+
+    GameObject starPrefab;
+    public Color starColor;
+
+    private StarsUI starsUI;
     public Color starFullColor;
     public Color starEmptyColor;
 
-    public Color swingEdgeMaterialColor;
-    public Color swingFillMaterialColor;
-
-
     Material swingEdgeMaterial;
     Material swingFillMaterial;
-
-    private Dictionary<string, string> palette;
+    public Color swingEdgeMaterialColor;
+    public Color swingFillMaterialColor;
 
     private void Awake()
     {
         starsUI = FindObjectOfType<StarsUI>();
         swingEdgeMaterial = (Resources.Load("TerrainMaterials/SwingEdgeMaterial") as Material);
         swingFillMaterial = (Resources.Load("TerrainMaterials/SwingFillMaterial") as Material);
+        heroPrefab = (Resources.Load("Prefabs/Hero") as GameObject);
+        goalPrefab = (Resources.Load("Prefabs/Goal") as GameObject);
+        starPrefab = (Resources.Load("Prefabs/Star") as GameObject);
     }
 
     private void Start()
     {
         UpdateColors();
+    }
+
+    private void Update()
+    {
+        if (Debug.isDebugBuild && updateColorsInUpdate)
+        {
+            UpdateColors();
+        }
     }
 
     private void UpdateColors()
@@ -41,5 +58,32 @@ public class ColorManager : MonoBehaviour
 
         swingEdgeMaterial.color = swingEdgeMaterialColor;
         swingFillMaterial.color = swingFillMaterialColor;
+
+        if (Debug.isDebugBuild && updateColorsInUpdate && FindObjectOfType<Hero>())
+        {
+            FindObjectOfType<Hero>().GetComponent<SpriteRenderer>().color = heroColor;
+        }
+        else
+        {
+            heroPrefab.GetComponent<SpriteRenderer>().color = heroColor;
+        }
+
+        if (Debug.isDebugBuild && updateColorsInUpdate && FindObjectOfType<Goal>())
+        {
+            FindObjectOfType<Goal>().GetComponent<SpriteRenderer>().color = goalColor;
+        }
+        else
+        {
+            goalPrefab.GetComponent<SpriteRenderer>().color = goalColor;
+        }
+
+        if (Debug.isDebugBuild && updateColorsInUpdate && FindObjectOfType<Star>())
+        {
+            FindObjectOfType<Star>().GetComponent<SpriteRenderer>().color = starColor;
+        }
+        else
+        {
+            starPrefab.GetComponent<SpriteRenderer>().color = starColor;
+        }
     }
 }
