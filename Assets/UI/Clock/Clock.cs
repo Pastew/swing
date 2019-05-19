1dfa2860513448cf1b4968e905e3ff9ca118415e
Pastew <kpastew@gmail.com>
2019-05-19 13:48:22 +0200

    Clock tick event

---
+++ Assets/UI/Clock/Clock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Clock : MonoBehaviour
+{
+    public UnityEvent clockTickEvent;
+
+    void Start()
+    {
+        if (clockTickEvent == null)
+            clockTickEvent = new UnityEvent();
+    }
+
+    public void AddOnTickListener(UnityAction clockTickSubscriber)
+    {
+        clockTickEvent.AddListener(clockTickSubscriber);
+    }
+
+    // This is invoked by animation
+    public void ClockTick()
+    {
+        clockTickEvent.Invoke();
+        print("Tick");
+    }
+}
