00cbcf51e2a58a2002fc1a478acd8d3e8ecd060e
Pastew <kpastew@gmail.com>
2019-05-28 22:39:02 +0200

    Camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 2f;
 
-    void Update()
+    float minX = -7, maxX = 7;
+    float minY = -6, maxY = 6;
+
+    public float maxCamX = 1, maxCamY = 1;
+
+    private void Update()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * speed);
+        Hero target = FindObjectOfType<Hero>();
+
+        if (target == null)
+            return;
+
+
+        Vector3 newPosition = new Vector3();
+        newPosition.z = -10;
+
+        newPosition.x = remap(minX, maxX, -maxCamX, maxCamX, target.transform.position.x);
+        newPosition.y = remap(minY, maxY, -maxCamX, maxCamY, target.transform.position.y);
+
+        transform.position = Vector3.Slerp(transform.position, newPosition, speed * Time.deltaTime);
+    }
+
+    public float remap(float a0, float a1, float b0, float b1, float a)
+    {
+        return b0 + (b1 - b0) * ((a - a0) / (a1 - a0));
     }
 }
