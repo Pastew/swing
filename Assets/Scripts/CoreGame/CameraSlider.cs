ae8387336009f4ccb512c27c21865d6b9f6bebf1
kamil.p <kamil.p@yggdrasilgaming.com>
2020-03-24 18:52:22 +0100

    added epic toon fx. Added death effect

---
+++ Assets/Scripts/CoreGame/CameraSlider.cs
@@ -0,0 +1,41 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace CoreGame
+{
+    public class CameraSlider : MonoBehaviour
+    {
+        [SerializeField] private float _slideOutSize = 1000;
+        [SerializeField] private Ease _ease = Ease.OutBounce;
+        [SerializeField] private float _duration = 0.5f;
+
+        private Camera _camera;
+        private float _startingCameraSize;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+            _startingCameraSize = _camera.orthographicSize;
+        }
+
+        public Tweener SlideIn()
+        {
+            return Slide(_startingCameraSize);
+        }
+
+        public Tweener SlideOut()
+        {
+            return Slide(_slideOutSize);
+        }
+
+        private Tweener Slide(float size)
+        {
+            return _camera.DOOrthoSize(size, _duration).SetEase(_ease);
+        }
+
+        public void SlideOutInstant()
+        {
+            _camera.orthographicSize = _slideOutSize;
+        }
+    }
+}
